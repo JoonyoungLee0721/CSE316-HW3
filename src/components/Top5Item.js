3e208f7b82b9c5d4691e21130bc54c8da56e6148
JoonyoungLee0721 <75687084+JoonyoungLee0721@users.noreply.github.com>
2021-10-19 07:19:34 +0900

    Add files via upload

---
+++ src/components/Top5Item.js
@@ -0,0 +1,115 @@
+import { React, useContext, useState } from "react";
+import { GlobalStoreContext } from '../store'
+/*
+    This React component represents a single item in our
+    Top 5 List, which can be edited or moved around.
+    
+    @author McKilla Gorilla
+*/
+function Top5Item(props) {
+    const { store } = useContext(GlobalStoreContext);
+    const [draggedTo, setDraggedTo] = useState(0);
+    const [ editActive, setEditActive ] = useState(false);
+    const [ text, setText ] = useState("");
+    function handleToggleEdit(event) {
+        event.stopPropagation();
+        setText(store.currentList.items[index]);
+        toggleEdit();
+    }
+
+    function toggleEdit() {
+        let newActive = !editActive;
+        if (newActive) {
+            store.setIsItemNameEditActive();
+        }
+        setEditActive(newActive);
+    }
+
+    function handleKeyPress(event) {
+        if (event.code === "Enter") {
+            let index = event.target.id.substring("item-".length);
+            let origin = store.currentList.items[index-1];
+            store.addChangeItemTransaction(index-1,origin,text);
+            toggleEdit();
+        }
+    }
+
+    function handleUpdateText(event) {
+        setText(event.target.value);
+    }
+    function handleDragStart(event) {
+        event.dataTransfer.setData("item", event.target.id);
+    }
+
+    function handleDragOver(event) {
+        event.preventDefault();
+    }
+
+    function handleDragEnter(event) {
+        event.preventDefault();
+        setDraggedTo(true);
+    }
+
+    function handleDragLeave(event) {
+        event.preventDefault();
+        setDraggedTo(false);
+    }
+
+    function handleDrop(event) {
+        event.preventDefault();
+        let target = event.target;
+        let targetId = target.id;
+        targetId = targetId.substring(target.id.indexOf("-") + 1);
+        let sourceId = event.dataTransfer.getData("item");
+        sourceId = sourceId.substring(sourceId.indexOf("-") + 1);
+        setDraggedTo(false);
+
+        // UPDATE THE LIST
+        store.addMoveItemTransaction(sourceId, targetId);
+    }
+
+    let { index } = props;
+    let itemClass = "top5-item";
+    if (draggedTo) {
+        itemClass = "top5-item-dragged-to";
+    }
+    let itemStatus = false;
+    if (store.isItemEditActive) {
+        itemStatus = true;
+    }
+    let item=
+        <div
+            id={'item-' + (index + 1)}
+            className={itemClass}
+            onDragStart={handleDragStart}
+            onDragOver={handleDragOver}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDrop={handleDrop}
+            draggable="true"
+        >
+            <input
+                type="button"
+                id={"edit-item-" + index }
+                className="list-card-button"
+                disabled={itemStatus}
+                value={"\u270E"}
+                onClick={handleToggleEdit}
+            />
+            {props.text}
+        </div>
+    if(editActive){
+        item=
+        <input
+        id={"item-" + (index+1)}
+        className={itemClass}
+        type='text'
+        onKeyPress={handleKeyPress}
+        onChange={handleUpdateText}
+        defaultValue={store.currentList.items[index]}
+        />;
+    }
+    return item;
+}
+
+export default Top5Item;
